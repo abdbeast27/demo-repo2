64f37c2959a0118d23eac37870d9557e1698361e
acachhi1 <acachhi1@visteon.com>
2023-11-02 13:37:54 +0530

    lkdnkl

--- abd.py
+++ abd.py
@@ -1,1 +1,11 @@
-print("hello")
+
+# Write a function called `search` which takes in 2 parameters:
+#     1. arr which is the array
+#     2. target which is the value to be search in the array
+def search(arr,target):
+     if target in arr:
+          print("exists")
+    else:
+        print("no")
+
+
